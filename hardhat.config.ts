0154c6c780ad96fb576240eca0eaf5d01c4bbf32
CabinBranis <branislav0203@mail.com>
2025-09-03 19:38:00 +0700

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@fhevm/hardhat-plugin";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+export default config;
